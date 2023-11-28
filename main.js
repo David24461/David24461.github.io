3610a3f70736b3c619f7eb3dd98f14fc915f7b37
David24461 <david24461@live.ytech.edu>
2023-11-28 10:25:43 -0500

    updated

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ const renderer = new THREE.WebGL1Renderer({
 
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
-camera.position.z = 80;
+camera.position.setZ(30)
+camera.position.setX(-3)
 
 
 //LIGHTS
@@ -32,7 +33,7 @@ scene.add(ambientLight)
 // const axesHelper = new THREE.AxesHelper(20, 20, 20)
 // scene.add(lightHelper, gridHelper, axesHelper)
 
-const geoPog = new THREE.CylinderGeometry(41.37, 41.37, 6, 100)
+const geoPog = new THREE.CylinderGeometry(20.37, 20.37, 3, 50)
 const texturePog = new THREE.TextureLoader().load('eevee.png');
 const matPog = new THREE.MeshStandardMaterial(
   {
@@ -45,6 +46,7 @@ const pog = new THREE.Mesh( geoPog, matPog);
 pog.rotation.x = 45;
 
 scene.add(pog);
+pog.scale.set(0.1, 0.1, 0.1)
 
 
 function newStar() {
@@ -60,16 +62,39 @@ function newStar() {
 Array(200).fill().forEach(newStar);
 
 const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg')
 
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
   new THREE.MeshStandardMaterial( {
-    map: moonTexture
+    map: moonTexture,
+    nomralMap: normalTexture
   })
 )
 
 scene.add(moon)
-moon.position.x = 10
+moon.position.z = 30;
+moon.position.setX(-10);
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  pog.rotation.x += 0.01;
+  pog.rotation.y += 0.013;
+  pog.rotation.z += 0.01;
+
+
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera
+moveCamera();
+
 
 const spaceTexture = new THREE.TextureLoader().load('space.jpg')
 scene.background = spaceTexture;
@@ -79,7 +104,7 @@ function animate(time) {
   requestAnimationFrame( animate );
 
   pog.rotation.x += 0.01;
-  pog.rotation.y += 0.005;
+  // pog.rotation.y += 0.005;
   pog.rotation.z += 0.01;
 
   renderer.render( scene, camera );
